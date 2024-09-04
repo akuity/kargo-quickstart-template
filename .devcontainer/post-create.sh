a576c33a94061f6763b1fa7f6128fa7b2224a58f
Christian Hernandez <christianh814@users.noreply.github.com>
2024-09-04 15:59:01 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,7 +4,7 @@
 echo "post-create start" >> ~/.status.log
 
 ## Create K3d cluster
-k3d cluster create kargo-quickstart| tee -a ~/.status.log
+k3d cluster create kargo-quickstart | tee -a ~/.status.log
 
 ## Update Repo With proper username
 bash .devcontainer/scripts/update-repo-for-workshop.sh | tee -a ~/.status.log
